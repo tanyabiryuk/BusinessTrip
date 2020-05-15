fd62a7cd22304ac64fb985180d59dc66ba2878d7
Tanya <tanyabiryuk7@gmail.com>
2020-05-15 18:45:34 +0300

    revert project. No merge conflicts

---
+++ Models/Lifecycle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusinessTripMyVersion.Models
+{
+    public class Lifecycle
+    {
+        // ID 
+        public int Id { get; set; }
+        // Дата открытия
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime Opened { get; set; }
+        // Дата распределения
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Distributed { get; set; }
+        // Дата обработки
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Proccesing { get; set; }
+        // Дата проверки
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Checking { get; set; }
+        // Дата закрытия
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Closed { get; set; }
+    }
+}
